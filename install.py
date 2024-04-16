857c071b6c06be3b3914459bf9941eb23a96fdc9
tyrellai <xiaobai@tyrellai.tech>
2024-04-16 12:57:42 +0800

    修复官方SD安装的环境无法解密的问题

--- install.py
+++ install.py
@@ -4,11 +4,10 @@ import os,time,json,pickle
 import shutil
 module_path = os.path.dirname(__file__)
 sys.path.insert(0, module_path)
-if not launch.is_installed("pyjwt"):
-    launch.run_pip("install pyjwt", "")
-if not launch.is_installed("cryptography"):
-    launch.run_pip("install cryptography", "")
-
+# if not launch.is_installed("pyjwt"):
+#     launch.run_pip("install pyjwt", "")
+# if not launch.is_installed("cryptography"):
+#     launch.run_pip("install cryptography", "")
 from scripts.cryp_gui_func import  uninstall_encryption
 uninstall_encryption()
 
