3dcde598a6fa5c1d4ea7a21960098b877c7585c7
tyrellai <xiaobai@tyrellai.tech>
2024-08-31 14:52:20 +0800

    适配最新的forge2.0

--- install.py
+++ install.py
@@ -8,6 +8,8 @@ sys.path.insert(0, module_path)
 #     launch.run_pip("install pyjwt", "")
 if not launch.is_installed("cryptography"):
    launch.run_pip("install cryptography", "")
+if not launch.is_installed("psutil"):
+   launch.run_pip("install psutil", "")
 from scripts.cryp_gui_func import  uninstall_encryption
 uninstall_encryption()
 
